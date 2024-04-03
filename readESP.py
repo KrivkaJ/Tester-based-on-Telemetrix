79b03877bc8fafb3488be3eed592e31c6cd776a6
Jan Křivka <hon.krivka@gmail.com>
2024-04-03 16:25:18 +0200

    reading_pins_by_stm_but_not_by_esp

--- readESP.py
+++ readESP.py
@@ -16,8 +16,8 @@ global_value = 0
 #GND
 list_stm_esp_value.append([makePin('F', 13), 23, 0])
 list_stm_esp_value.append([makePin('F', 14), 22, 0])
-list_stm_esp_value.append([makePin('F', 15), 1, 0])
-list_stm_esp_value.append([makePin('E', 0), 3, 0])
+#list_stm_esp_value.append([makePin('F', 15), 1, 0])
+#list_stm_esp_value.append([makePin('E', 0), 3, 0])
 list_stm_esp_value.append([makePin('E', 1), 21, 0])
 #GND
 list_stm_esp_value.append([makePin('E', 2), 19, 0])
@@ -29,13 +29,13 @@ list_stm_esp_value.append([makePin('E', 7), 4, 0])
 list_stm_esp_value.append([makePin('E', 8), 0, 0])
 list_stm_esp_value.append([makePin('E', 9), 2, 0])
 list_stm_esp_value.append([makePin('E', 10), 15, 0])
-#list_stm_esp_value.append([makePin('E', 11), 8, 0])
+#list_stm_esp_value.append([makePin('E', 11), 8, 0]) pin shared with the flash memory
 #list_stm_esp_value.append([makePin('E', 12), 7, 0])
 #list_stm_esp_value.append([makePin('E', 13), 6, 0])
 
 #3v3
 #EN
-#list_stm_esp_value.append([makePin('C', 10), 36, 0]) prej tyto 4 input only
+#list_stm_esp_value.append([makePin('C', 10), 36, 0]) these 4 input only
 #list_stm_esp_value.append([makePin('C', 11), 39, 0])
 #list_stm_esp_value.append([makePin('C', 12), 34, 0])
 #list_stm_esp_value.append([makePin('C', 13), 35, 0])
@@ -48,11 +48,16 @@ list_stm_esp_value.append([makePin('D', 3), 14, 0])
 list_stm_esp_value.append([makePin('D', 4), 12, 0])
 #GND
 list_stm_esp_value.append([makePin('D', 5), 13, 0])
-#list_stm_esp_value.append([makePin('D', 6), 9, 0])
+#list_stm_esp_value.append([makePin('D', 6), 9, 0]) pin shared with the flash memory
 #list_stm_esp_value.append([makePin('D', 7), 10, 0])
 #list_stm_esp_value.append([makePin('D', 8), 11, 0])
 
-
+list_esp_read_stm = []
+list_esp_read_stm.append([makePin('C', 10), 36, 0]) #these 4 input only on stm
+list_esp_read_stm.append([makePin('C', 11), 39, 0])
+list_esp_read_stm.append([makePin('C', 12), 34, 0])
+list_esp_read_stm.append([makePin('C', 13), 35, 0])
+list_esp_read_stm.append([makePin('F', 13), 23, 0])
 
 
 # Callback data indices
@@ -102,21 +107,35 @@ def digital_in(my_board, pin):
     # my_board.enable_digital_reporting(12)
 
 
-pinSTM = makePin("F",13)
-pinESP = 23
-boardESP.set_pin_mode_digital_output(pinESP)
+#pinSTM = makePin("F",13)
+#pinESP = 23
+#boardESP.set_pin_mode_digital_output(pinESP)
 #boardSTM.set_pin_mode_digital_input(pinSTM)
 
 boardSTM.disable_all_reporting()
+boardESP.disable_all_reporting()
+
+for esp_read_stm in list_stm_esp_value:
+    boardSTM.set_pin_mode_digital_output(esp_read_stm[0])
+    digital_in(boardESP, esp_read_stm[1])
+    boardSTM.digital_write(esp_read_stm[0], 1)
+    time.sleep(1)
+    boardSTM.digital_write(esp_read_stm[0], 0)
+    time.sleep(1)
+    print('existuju')
+    
+
+boardSTM.disable_all_reporting()
+boardESP.disable_all_reporting()
 
 for stm_esp_value in list_stm_esp_value:
     boardESP.set_pin_mode_digital_output(stm_esp_value[1])
-    boardESP.digital_write(stm_esp_value[1], 1)
-    time.sleep(1)
     digital_in(boardSTM, stm_esp_value[0])
+    boardESP.digital_write(stm_esp_value[1], 1)
     time.sleep(1)
     stm_esp_value[2] = global_value
     boardESP.digital_write(stm_esp_value[1], 0)
+    time.sleep(1)
     print(stm_esp_value)
 
 """    
